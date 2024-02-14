fdbb64551a3d7b43f464301562b0668aca2255ec
socathie <socathie@gmail.com>
2024-02-14 05:20:57 +0000

    Update circuit.circom and ezkl settings

--- mnist/circom_1_compile.sh
+++ mnist/circom_1_compile.sh
@@ -1,8 +1,10 @@
 # compile circuit.circom in all model_{1..7} directories
 
+# before compiling, replace "component main = Model();" with "component main {public [in]} = Model();"
 for i in {1..7}
 do
     cd model_$i
+    sed -i 's/component main = Model();/component main {public [in]} = Model();/g' circuit.circom
     circom circuit.circom --r1cs --wasm
     cd ..
 done
--- mnist/ezkl_0_settings.sh
+++ mnist/ezkl_0_settings.sh
@@ -1,7 +1,7 @@
 for i in {1..7}
 do
     cd model_$i
-    ezkl gen-settings -M model.onnx
+    ezkl gen-settings -M model.onnx --input-visibility public  --param-visibility private
     ezkl calibrate-settings -M model.onnx -D ../ezkl_input.json --target resources
     cd ..
 done
