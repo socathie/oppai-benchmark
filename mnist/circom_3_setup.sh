3e2d5f4500417c5944c62e2f9f1cf619d7947899
socathie <socathie@gmail.com>
2024-02-14 17:49:45 +0000

    Update trusted setup and download files

--- mnist/circom_3_setup.sh
+++ mnist/circom_3_setup.sh
@@ -1,9 +1,13 @@
 # trusted setup in model_{1..7} directories
+export NODE_OPTIONS=--max_old_space_size=131072
+sudo sysctl -w vm.max_map_count=655300
+
+# model_1 use 24
+# others use 25
 for i in {1..7}
 do
     cd model_$i
-    snarkjs powersoftau prepare phase2 ../powersOfTau28_hez_final_26.ptau pot26_final.ptau -v
-    snarkjs groth16 setup circuit.r1cs pot26_final.ptau circuit_0000.zkey
+    snarkjs groth16 setup circuit.r1cs ../powersOfTau28_hez_final_25.ptau circuit_0000.zkey
     snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey --name="1st Contributor Name" -v
     cd ..
 done
--- mnist/wget.sh
+++ mnist/wget.sh
@@ -1,1 +1,2 @@
-wget https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_26.ptau
+wget https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_24.ptau
+wget https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_25.ptau
