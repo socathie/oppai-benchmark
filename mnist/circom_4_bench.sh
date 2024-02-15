6792095aa3286f35e01b328c7e056367e131c329
socathie <socathie@gmail.com>
2024-02-15 10:02:01 +0000

    Add benchmarking results for circom models

--- mnist/circom_4_bench.sh
+++ mnist/circom_4_bench.sh
@@ -1,6 +1,9 @@
+export NODE_OPTIONS=--max_old_space_size=524288
+sudo sysctl -w vm.max_map_count=655300
+
 for i in {1..7}
 do
-    cd model_$i/circuit_js
+    cd model_$i
     /usr/bin/time -o ../bench/circom_bench_$i.out -v snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json
-    cd ../..
+    cd ..
 done
