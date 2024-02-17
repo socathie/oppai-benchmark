0f378890f1fd19ddf1aff851429aca09a91d9282
socathie <socathie@gmail.com>
2024-02-17 02:20:23 +0000

    Add benchmarking code to measure the execution time of op models

--- mnist/op_bench.py
+++ mnist/op_bench.py
@@ -5,13 +5,16 @@ os.environ['CUDA_VISIBLE_DEVICES'] = ''
 import tensorflow as tf
 import numpy as np
 
-model = tf.keras.models.load_model(f'op_model.h5')
+for i in range(1,7):
+    model = tf.keras.models.load_model(f'model_{i}/op_model.h5')
 
-# create random array based on model input shape
-input_shape = model.layers[0].input.shape[1:]
+    # create random array based on model input shape
+    input_shape = model.layers[0].input.shape[1:]
 
-# create random input
-input_data = np.random.rand(1, *input_shape)
+    # create random input
+    input_data = np.random.rand(1, *input_shape)
 
-# run the model
-model.predict(input_data)
+    # run the model and time it
+    import timeit
+    time = timeit.timeit(lambda: model.predict(input_data, verbose=False), number=100)
+    print(f"Time taken to run the op model {i}:", time/100)
